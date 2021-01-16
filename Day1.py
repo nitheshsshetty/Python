29a9feb45ff84002cd42620316e8320e5f31bbe9
Nithesh Shetty <17782014+nitheshsshetty@users.noreply.github.com>
2021-01-16 20:25:26 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Welcome to Band name generator\n")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("Please enter your city name :\n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("Please enter your pet name :\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("your Band name is {}{}".format(city_name,pet_name) )
